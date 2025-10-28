ac90a59f06057346eca34822124d960042d26e20
Emil Christiansen <emil.christiansen@ntnu.no>
2025-10-28 15:01:51 +0100

    Created example submodule

---
+++ NORTEMtools/Example/utils.py
@@ -0,0 +1,10 @@
+"""
+Docstring for NORTEMtools.Example.utils
+
+This file contains the code your submodule. You can import other stuff NORTEMtools if you need them, such as the `logger`.
+"""
+
+from NORTEMtools import logger
+
+def foo():
+    logger.info('foo') #Log the string "foo".
