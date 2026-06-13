991fe95564858c20ab6758f1ee0a041305548045
Emil Christiansen <emil.christiansen@ntnu.no>
2026-06-13 22:59:58 +0200

    Update __init__.py

--- NORTEMtools/Emil/jh5/__init__.py
+++ NORTEMtools/Emil/jh5/__init__.py
@@ -1,3 +1,17 @@
-from .io import JH5Reader, load_JEOL_worksheet, load_jh5, jh5_to_hspy
+from .io import (
+    JH5Reader,
+    load_JEOL_worksheet,
+    load_jh5,
+    jh5_to_hspy,
+    load_JEOL_EDS,
+    load_eds_part,
+)
 
-__all__ = ["JH5Reader", "load_JEOL_worksheet", "load_jh5", "jh5_to_hspy", "load_JEOL_EDS", "load_eds_part"]
+__all__ = [
+    "JH5Reader",
+    "load_JEOL_worksheet",
+    "load_jh5",
+    "jh5_to_hspy",
+    "load_JEOL_EDS",
+    "load_eds_part",
+]
